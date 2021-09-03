bd8e06a0e2fa63cfdb4cccc2223cd9bb016c284a
Nobody4931 <ardenxie1@gmail.com>
2021-09-02 23:19:16 -0400

    migrated dobject group dm channel

--- lib/dcache/dobjects.hpp
+++ lib/dcache/dobjects.hpp
@@ -53,5 +53,6 @@ typedef std::shared_ptr<disc_user_t>               disc_user_ptr_t;
 #include "dcache/dobjects/channel_text.hpp"
 #include "dcache/dobjects/channel_voice.hpp"
 #include "dcache/dobjects/channel_dm.hpp"
+#include "dcache/dobjects/channel_groupdm.hpp"
 
 #endif // DOBJECTS_DECLARATIONS_ONLY
---
+++ lib/dcache/dobjects/channel_groupdm.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+
+#define DOBJECTS_DECLARATIONS_ONLY
+
+#include <nlohmann/json.hpp>
+
+#include "dcache/dobjects.hpp"
+#include "dcache/dobjects/channel_dm.hpp"
+
+
+class disc_channel_groupdm_t : public disc_channel_dm_t {
+public:
+	disc_channel_groupdm_t()
+		: disc_channel_dm_t() {}
+
+	disc_channel_groupdm_t( nlohmann::json& channel_data, disc_guild_ptr_t parent )
+		: disc_channel_dm_t( channel_data, parent )
+	{
+		// TODO: This
+	}
+
+public:
+	disc_user_ptr_t owner;
+};
