590c3f62bd668d59e455ae78533b91e70c9bef34
Nobody4931 <ardenxie1@gmail.com>
2021-09-02 22:49:31 -0400

    migrated dobject category channel

--- lib/dcache/dobjects.hpp
+++ lib/dcache/dobjects.hpp
@@ -49,5 +49,6 @@ typedef std::shared_ptr<disc_user_t>               disc_user_ptr_t;
 #include "dcache/dobjects/guild.hpp"
 #include "dcache/dobjects/role.hpp"
 #include "dcache/dobjects/channel_base.hpp"
+#include "dcache/dobjects/channel_category.hpp"
 
 #endif // DOBJECTS_DECLARATIONS_ONLY
