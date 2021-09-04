abbdd3b9fbb4c499522e1514fd717b67a8e2c456
Nobody4931 <ardenxie1@gmail.com>
2021-09-04 15:46:31 -0400

    guild caching stuff

--- lib/dcache/dobjects/guild.hpp
+++ lib/dcache/dobjects/guild.hpp
@@ -8,6 +8,7 @@
 
 #include <nlohmann/json.hpp>
 
+#include "util/str2num.hpp"
 #include "dcache/dtypes.hpp"
 #include "dcache/dobjects.hpp"
 
@@ -17,8 +18,28 @@ public:
 	disc_guild_t()
 		: id( 0 ) {}
 
-	disc_guild_t( nlohmann::json& guild_data ) {
-		// TODO: This
+	disc_guild_t( nlohmann::json& guild_data )
+		: id( str_to_integer<dsnowflake_t>( guild_data["id"].get_ref<std::string&>() ) )
+	{ m_update( guild_data ); }
+
+public:
+	// was going to toss this into a source file but it was
+	// originally supposed to be the constructor so nah
+	inline void m_update( nlohmann::json& guild_data ) {
+		// Update primitive fields
+		name = guild_data["name"].get<std::string>();
+
+		auto unavailable_it = guild_data.find( "unavailable" );
+		unavailable = unavailable_it != guild_data.end() ? unavailable_it->get<bool>() : false;
+
+		auto owner_it = members.find( str_to_integer<dsnowflake_t>( guild_data["owner_id"].get_ref<std::string&>() ) );
+		if ( owner_it != members.end() ) {
+			owner = owner_it->second;
+		}
+
+		// TODO: Force load owner if not found?
+		// - For large guilds with like 1000+ members
+		// - Probably not, doubt I'll ever even talk in those servers, much less handle commands there
 	}
 
 public:
